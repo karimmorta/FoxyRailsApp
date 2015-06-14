4e518b66e1caf35992bf2f2f12dd404cf6ae5b28
littletaz <az7@users.noreply.github.com>
2015-06-14 16:07:37 +0200

    add video homepage

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -6,3 +6,22 @@ function hide(div) {
 	document.getElementById(div).style.display='none';
 	return false;
 }
+
+
+function start_video(e) {
+	e.preventDefault();
+	console.log('test');
+    var video = document.getElementById("mockup_video");
+	$(e.currentTarget).css('display', 'none');
+	$('.overlay_video').css('display', 'none');
+	$('#mockup_video').css('display', 'block');
+	video.play();
+};
+
+
+
+
+$(document).ready(function() {
+	var btn_video = $('.button');
+	btn_video.on('click', start_video);
+});
