1a1905417f170632221802e4c7e0be916088b42f
littletaz <az7@users.noreply.github.com>
2015-06-14 15:16:28 +0200

    fix search script call

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -10,69 +10,74 @@ var id =1;
 var id_quarter =2;
 
 function datamind(){
-  $.getJSON("stats.json", function(data) {
-    donnee = data;
-    offensive_ranking(null);
-    quarter_stats(null);
-    btn.on('click', quarter_stats);
-    btn_search.on('click', open_search);
-  });
+	$.getJSON("stats.json", function(data) {
+		donnee = data;
+		offensive_ranking(null);
+		quarter_stats(null);
+		btn.on('click', quarter_stats);
+		btn_search.on('click', open_search);
+	});
 }
 
 function open_search(e) {
-    e.preventDefault();
-    $('.search_open').removeClass(' display_off');
-    $('.search_open').addClass(' display_on');
+	e.preventDefault();
+	if ( $(e.currentTarget).parent().hasClass('active_search')) {
+		$('.search_open').removeClass(' display_on');
+		$(e.currentTarget).parent().removeClass(' active_search');
+	} else {
+		$('.search_open').addClass(' display_on'); 
+		$(e.currentTarget).parent().addClass(' active_search');
+	};
 };
 
 function quarter_stats(e) {
-  if(e != null && e != undefined && e != '') {    
-    $('.q_subnav li a').removeClass('active');
-    $(e.currentTarget).addClass(' active');
-    e.preventDefault();
-    id_quarter = $(e.currentTarget).attr('data-id');
-    console.log(id);
-  };
-  for (var i = 1; i <= 2; i++) {
-    for (var w = 0; w <= 3; w++) {
-      // h récupere la data pour j
-      h = donnee.data['team'+i]['stats'+id_quarter][w]['q'+(w+1)];
+	if(e != null && e != undefined && e != '') {    
+		$('.q_subnav li a').removeClass('active');
+		$(e.currentTarget).addClass(' active');
+		e.preventDefault();
+		id_quarter = $(e.currentTarget).attr('data-id');
+		console.log(id);
+	};
+	for (var i = 1; i <= 2; i++) {
+		for (var w = 0; w <= 3; w++) {
+			// h récupere la data pour j
+			h = donnee.data['team'+i]['stats'+id_quarter][w]['q'+(w+1)];
 
-      // Applique le style
-      $('.q_content .team'+i +'.0'+(w+1)).css('height', h);
-    };
-  };
+			// Applique le style
+			$('.q_content .team'+i +'.0'+(w+1)).css('height', h);
+		};
+	};
 };
 
 function offensive_ranking(e){
-  if(e != null && e != undefined && e != '') {
-    e.preventDefault();
-    id = $(e.currentTarget).attr('data-id');
-    console.log(id);
-  };
+	if(e != null && e != undefined && e != '') {
+		e.preventDefault();
+		id = $(e.currentTarget).attr('data-id');
+		console.log(id);
+	};
 
-  // var result = change_color(id);
-  // console.log(result);
+	// var result = change_color(id);
+	// console.log(result);
 
-  for (var i = 1; i <= 2; i++) {
-    for (var j = 0; j <= 3; j++) {
+	for (var i = 1; i <= 2; i++) {
+		for (var j = 0; j <= 3; j++) {
 
-      // h récupere la data pour j
-      h = donnee.data['team'+i]['stats'+id][j]['q'+(j+1)];
+			// h récupere la data pour j
+			h = donnee.data['team'+i]['stats'+id][j]['q'+(j+1)];
 
-      // Applique le style
-      $('.o_content .team'+i +'.0'+(j+1)).css('height', h);
-    };
-      
-  };
+			// Applique le style
+			$('.o_content .team'+i +'.0'+(j+1)).css('height', h);
+		};
+			
+	};
 
 
-  return;
+	return;
 };
 
 $(document).ready(function() {
-  btn = $('.target');
-  btn_search = $('.search');
-  datamind();
+	btn = $('.target');
+	btn_search = $('.search');
+	datamind();
 });
 
