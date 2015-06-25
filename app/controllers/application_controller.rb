35da943c85dea279fb4bb0124fb3ea8ee6adec8e
Karim Mortabit <karim.mortabit@gmail.com>
2015-06-26 01:59:12 +0200

    Registration, new images, new data for dashboard

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,15 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :exception  
+  protect_from_forgery with: :null_session, if: Proc.new { |c| c.request.format.json? }
 
   before_filter :define_games
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email, :password, :password_confirmation) }
+  end
 
   def define_games
     @games = []
--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,15 +1,19 @@
 class DashboardController < ApplicationController
   def index
-    if params[:game_id]
-      @game = Game.find(params[:game_id])
-    else
-      @game = Game.last
-    end
+  if params[:game_id]
+    @game = Game.find(params[:game_id])
     @picks = @game.picks.where(game: @game)
     @tipsters = @game.picks.map(&:tipster)
     if !@game.picks.blank?
       @winning_team = @game.outcome.first
       @outcome = @game.outcome.second
-    end
+    else
+      redirect_to games_path
+  end
+  else 
+    redirect_to games_path
+  end
+  rescue ActiveRecord::RecordNotFound
+    redirect_to games_path
   end
 end
